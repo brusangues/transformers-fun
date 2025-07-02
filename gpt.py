b7aee74edf20d885956f4f4715e26a7474a9bf91
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2025-07-01 23:00:50 -0300

    changing some parameters

--- gpt.py
+++ gpt.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 500
+batch_size = 16 # how many independent sequences will we process in parallel?
+block_size = 128 # what is the maximum context length for predictions?
+max_iters = 1000
+eval_interval = 10
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
+print(f"{device=}")
+eval_iters = 100
 n_embd = 384
 n_head = 6
 n_layer = 6
