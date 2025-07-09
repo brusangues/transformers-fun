8f6efad8255c75e02705e3a2644f50951c51f7fd
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2025-07-09 17:13:43 -0300

    criando algumas pastas e organizando o código

---
+++ clean_text.py
@@ -0,0 +1,47 @@
+import re
+from unidecode import unidecode
+
+# Lendo e olhando para os caracteres do texto de Shakespeare
+input_file = "data/shakespeare.txt"
+print(f"{input_file=}")
+with open(input_file, "r", encoding="utf-8") as f:
+    text = f.read()
+chars = sorted(list(set(text)))
+print(f"{len(text)=} {len(chars)=}\n{chars=}")
+
+# Obra de lovecraft em português
+input_file = "data/lovecraft_ptbr.txt"
+print(f"{input_file=}")
+with open(input_file, "r", encoding="utf-8") as f:
+    text = f.read()
+chars = sorted(list(set(text)))
+print(f"{len(text)=} {len(chars)=}\n{chars=}")
+
+# Limpando trechos de início e fim do livro
+# id_start = re.search("DAGON", text).span(0)[0]
+# print(text[id_start : id_start + 1000])
+# id_end = re.search("FONTES DOS TEXTOS", text).span(0)[0] - 1
+# print(text[-1000 + id_end : id_end])
+# text_cropped = text[id_start:id_end]
+
+
+# Limpando algumas repetições de espaços, quebras de linha e tabulações
+def clean_text(s):
+    s = unidecode(s)
+    s = re.sub("\n+", "\n", s)
+    s = re.sub("\t+", "\t", s)
+    s = re.sub(" +", " ", s)
+    return s
+
+
+text_clean = clean_text(text)
+chars = sorted(list(set(text_clean)))
+print(f"{len(text_clean)=} {len(chars)=}\n{chars=}")
+print(f"{text_clean[:1000]=}")
+
+# Salvando o texto limpo
+output_file = "data/lovecraft_ptbr_clean.txt"
+with open(output_file, "w") as f:
+    f.write(text_clean)
+
+print(f"\n\nTexto limpo salvo em {output_file}")
--- gpt.py
+++ gpt.py
@@ -3,28 +3,29 @@ import torch.nn as nn
 from torch.nn import functional as F
 from tqdm import tqdm
 
+# 300 mb
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 128 # what is the maximum context length for predictions?
-max_iters = 3_000
+batch_size = 160  # how many independent sequences will we process in parallel?
+block_size = 128  # what is the maximum context length for predictions?
+max_iters = 3000
 eval_interval = 100
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+learning_rate = 3e-3
+device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"{device=}")
-eval_iters = 128
+eval_iters = 30
 n_embd = 384
-n_head = 2
+n_head = 4
 n_layer = 2
-dropout = 0.2
-INPUT_FILE = "lovecraft_clean.txt"
-N_TOKENS_GENERATE = 10_000
-LOAD = True
+dropout = 0.15
+INPUT_FILE = "data/lovecraft_ptbr_clean.txt"
+N_TOKENS_GENERATE = 1000
+LOAD = False
 # ------------
 
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open(INPUT_FILE, 'r', encoding='utf-8') as f:
+with open(INPUT_FILE, "r", encoding="utf-8") as f:
     text = f.read()
 print(f"{text[:10_000]=}")
 
@@ -36,32 +37,38 @@ vocab_size = len(chars)
 print(f"{vocab_size=}")
 print(f"{-torch.log(torch.ones(1)/vocab_size)=}")
 # create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [
+    stoi[c] for c in s
+]  # encoder: take a string, output a list of integers
+decode = lambda l: "".join(
+    [itos[i] for i in l]
+)  # decoder: take a list of integers, output a string
 
 # Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data)) # first 90% will be train, rest val
+n = int(0.9 * len(data))  # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
+
 
 # data loading
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
+    data = train_data if split == "train" else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
+
 
 @torch.no_grad()
 def estimate_loss():
     out = {}
     model.eval()
-    for split in ['train', 'val']:
+    for split in ["train", "val"]:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
             X, Y = get_batch(split)
@@ -71,36 +78,40 @@ def estimate_loss():
     model.train()
     return out
 
+
 class Head(nn.Module):
-    """ one head of self-attention """
+    """one head of self-attention"""
 
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         # input of size (batch, time-step, channels)
         # output of size (batch, time-step, head size)
-        B,T,C = x.shape
-        k = self.key(x)   # (B,T,hs)
-        q = self.query(x) # (B,T,hs)
+        B, T, C = x.shape
+        k = self.key(x)  # (B,T,hs)
+        q = self.query(x)  # (B,T,hs)
         # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = (
+            q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
+        )  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, T, T)
+        wei = F.softmax(wei, dim=-1)  # (B, T, T)
         wei = self.dropout(wei)
         # perform the weighted aggregation of the values
-        v = self.value(x) # (B,T,hs)
-        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        v = self.value(x)  # (B,T,hs)
+        out = wei @ v  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
 
+
 class MultiHeadAttention(nn.Module):
-    """ multiple heads of self-attention in parallel """
+    """multiple heads of self-attention in parallel"""
 
     def __init__(self, num_heads, head_size):
         super().__init__()
@@ -113,8 +124,9 @@ class MultiHeadAttention(nn.Module):
         out = self.dropout(self.proj(out))
         return out
 
+
 class FeedFoward(nn.Module):
-    """ a simple linear layer followed by a non-linearity """
+    """a simple linear layer followed by a non-linearity"""
 
     def __init__(self, n_embd):
         super().__init__()
@@ -128,8 +140,9 @@ class FeedFoward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+
 class Block(nn.Module):
-    """ Transformer block: communication followed by computation """
+    """Transformer block: communication followed by computation"""
 
     def __init__(self, n_embd, n_head):
         # n_embd: embedding dimension, n_head: the number of heads we'd like
@@ -145,6 +158,7 @@ class Block(nn.Module):
         x = x + self.ffwd(self.ln2(x))
         return x
 
+
 class GPTLanguageModel(nn.Module):
 
     def __init__(self):
@@ -152,8 +166,10 @@ class GPTLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.blocks = nn.Sequential(
+            *[Block(n_embd, n_head=n_head) for _ in range(n_layer)]
+        )
+        self.ln_f = nn.LayerNorm(n_embd)  # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
         # better init, not covered in the original GPT video, but important, will cover in followup video
@@ -171,19 +187,19 @@ class GPTLanguageModel(nn.Module):
         B, T = idx.shape
 
         # idx and targets are both (B,T) tensor of integers
-        tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
-        x = tok_emb + pos_emb # (B,T,C)
-        x = self.blocks(x) # (B,T,C)
-        x = self.ln_f(x) # (B,T,C)
-        logits = self.lm_head(x) # (B,T,vocab_size)
+        tok_emb = self.token_embedding_table(idx)  # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T,C)
+        x = tok_emb + pos_emb  # (B,T,C)
+        x = self.blocks(x)  # (B,T,C)
+        x = self.ln_f(x)  # (B,T,C)
+        logits = self.lm_head(x)  # (B,T,vocab_size)
 
         if targets is None:
             loss = None
         else:
             B, T, C = logits.shape
-            logits = logits.view(B*T, C)
-            targets = targets.view(B*T)
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
             loss = F.cross_entropy(logits, targets)
 
         return logits, loss
@@ -196,22 +212,23 @@ class GPTLanguageModel(nn.Module):
             # get the predictions
             logits, loss = self(idx_cond)
             # focus only on the last time step
-            logits = logits[:, -1, :] # becomes (B, C)
+            logits = logits[:, -1, :]  # becomes (B, C)
             # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1) # (B, C)
+            probs = F.softmax(logits, dim=-1)  # (B, C)
             # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
             # append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
         return idx
+
 
 model = GPTLanguageModel()
 m = model.to(device)
 if LOAD:
-    m.load_state_dict(torch.load('gpt_model_v2.pth', map_location=device))
+    m.load_state_dict(torch.load("gpt_model_v2.pth", map_location=device))
     print("Model loaded from 'gpt_model_v2.pth'")
 # print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+print(sum(p.numel() for p in m.parameters()) / 1e6, "M parameters")
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -221,10 +238,12 @@ for iter in tqdm(range(max_iters), total=max_iters):
     # every once in a while evaluate the loss on train and val sets
     if iter % eval_interval == 0 or iter == max_iters - 1:
         losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        print(
+            f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}"
+        )
 
     # sample a batch of data
-    xb, yb = get_batch('train')
+    xb, yb = get_batch("train")
 
     # evaluate the loss
     logits, loss = model(xb, yb)
@@ -233,10 +252,12 @@ for iter in tqdm(range(max_iters), total=max_iters):
     optimizer.step()
 
 # save model
-torch.save(m.state_dict(), 'gpt_model_v2.pth')
+torch.save(m.state_dict(), "gpt_model_v2.pth")
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-generated_text = decode(m.generate(context, max_new_tokens=N_TOKENS_GENERATE)[0].tolist())
+generated_text = decode(
+    m.generate(context, max_new_tokens=N_TOKENS_GENERATE)[0].tolist()
+)
 print(generated_text)
-open('more_v2.txt', 'w').write(generated_text)
+open("more_v2.txt", "w").write(generated_text)
